5916324d54c73ce7dcab2388ddddaf7b0ff152e6
roshan1209 <roshan1209britto@gmail.com>
2025-07-28 08:54:22 +0530

    - remove unwanted code.

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -4,10 +4,6 @@ import Header from "@/components/Header";
 import Footer from "@/components/Footer";
 
 function MyApp({ Component, pageProps }) {
-  useEffect(() => {
-    const theme = localStorage.getItem('theme') || 'light';
-    document.documentElement.classList.toggle('dark', theme === 'dark');
-  }, []);
 
   return (
       <div className="bg-red-50 text-black flex flex-col gap-4">
